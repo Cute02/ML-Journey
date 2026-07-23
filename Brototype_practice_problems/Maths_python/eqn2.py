0726548102a3c65bd5508f3573de96311beb11d8
Cute02 <maryneha248@gmail.com>
2026-07-23 12:48:37 +0530

    Variable relationship

---
+++ Brototype_practice_problems/Maths_python/eqn2.py
@@ -0,0 +1,9 @@
+"""
+Brototype's Colab Pro+ costs ₹90/hour plus a flat ₹200 platform fee.
+ Write the equation for total_cost, then find the cost for 8 hours.
+"""
+
+hrs=int(input("Enter the number of hours: "))
+total_cost=90*hrs+200
+print("Brototype's Colab Pro+ costs  ", total_cost)
+#Brototype's Colab Pro+ costs   920
